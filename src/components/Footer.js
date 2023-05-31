f66e34ba7a35558f3ac8780d4cd0bdf915da732e
Sunjang1009 <sunjang1009@gmail.com>
2023-05-31 18:13:38 -0400

    added emoticons at Footer Navbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,18 @@
 import Navbar from "react-bootstrap/Navbar";
+import { FaInstagram } from "react-icons/fa"
+import { FaTwitter } from "react-icons/fa"
+import { FaFacebook } from "react-icons/fa"
+import { FaYoutube } from "react-icons/fa"
 
 function Footer(){
     return(
         <>
             <Navbar className="footer-container" fixed="bottom">
-                Connect with us 
+                Connect with us{'\u00A0'}{'\u00A0'}
+                <FaInstagram />{'\u00A0'}{'\u00A0'}
+                <FaTwitter />{'\u00A0'}{'\u00A0'}
+                <FaFacebook />{'\u00A0'}{'\u00A0'}
+                <FaYoutube />
             </Navbar>
         </>
     )
