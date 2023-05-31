2e0a430e8e99a4badde2fb2f0f0bf07d58c8c601
Sunjang1009 <sunjang1009@gmail.com>
2023-05-31 11:53:26 -0400

    changed home page img with canva img

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,8 @@
 function Home(){
     return(
-        <div>
-            {/* <h1>Home Page</h1> */}
-            <img className="home-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-SDAaDv2MYWsFxhPvsVYyCo8HwyJIkBQ2YqbD_UbTLA&usqp=CAU&ec=48665701"
+        <div className="home-container">
+           
+            <img className="home-image" src="/HomePageImg.png"
             alt="home"
             />
         </div>
